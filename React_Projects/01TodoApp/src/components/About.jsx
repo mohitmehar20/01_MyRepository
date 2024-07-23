4b0bb33f76d09bcc0f066cd0372c77af58380934
mohitmehar20 <mohitmehar.2002@gmail.com>
2024-07-23 23:21:02 +0530

    made the todo app more responsive

--- React_Projects/01TodoApp/src/components/About.jsx
+++ React_Projects/01TodoApp/src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='flex-col bg-gray-500 p-10 w-[70%] mt-3 m-auto rounded-2xl min-h-[87vh]' >
+    <div className='w-full flex-col bg-gray-500 p-5 sm:p-8 sm:w-[70%] sm:mt-3 m-auto rounded-2xl min-h-[87vh]' >
       <h1 className='text-center font-bold text-3xl'>About Our ToDo App</h1>
       <p className='text-xl m-3'>Welcome to our ToDo App, your ultimate productivity companion designed to help you organize your life efficiently and effortlessly. Whether you are managing personal tasks, professional projects, or daily chores, our app provides a seamless and intuitive experience to ensure you stay on top of everything.</p>
       <h2 className=' text-center font-bold text-2xl'>Key Features :</h2>
--- React_Projects/01TodoApp/src/components/TodoList.jsx
+++ React_Projects/01TodoApp/src/components/TodoList.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import edit from '../img/pencil.svg'
+import remove from '../img/trash.svg'
 
 
 
@@ -37,7 +39,7 @@ function TodoList() {
     const [EnableEdit, setEnableEdit] = useState(false)
     const handleEdit = (index, id) => {
         setEnableEdit(true)
-        setUpdateIndex(index) 
+        setUpdateIndex(index)
         const t = Todos.filter(i => i.id === id)
         setTodo(t[0].Todo)
         saveToLocalStorage()
@@ -59,13 +61,15 @@ function TodoList() {
     }
 
     return (
-        <div className=' bg-gray-500 w-[70%] mt-3 m-auto rounded-2xl min-h-[87vh] '>
-            <div className='flex gap-4 items-center p-5'>
-                <h2 className='font-bold text-xl'>Add a Todo</h2>
-                <input value={Todo} onChange={(e) => setTodo(e.target.value)} type="text" className='rounded-lg p-2 w-[40%]' />
-                <button type='submit' onClick={addTodo} className={ EnableEdit ? 'hidden' : ' bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white  '}>Add</button>
+        <div className=' bg-gray-500 sm:w-[70%] mt-3 m-auto rounded-2xl min-h-[87vh] w-[100%]'>
+            <div className='sm:flex-row sm:justify-center flex-col items-center p-5'>
+                <h2 className='font-bold text-xl mb-3'>Add a Todo</h2>
+                <div className='flex w-full gap-1'>
+                    <input value={Todo} onChange={(e) => setTodo(e.target.value)} type="text" className='rounded-lg p-2 w-[782px] focus-visible:decoration-none' />
+                    <button type='submit' onClick={addTodo} className={EnableEdit ? 'hidden' : ' bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white ml-1 '}>Add</button>
 
-                <button type='submit' onClick={saveChange} className={ EnableEdit ? ' bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white  ':'hidden'}>Save</button>
+                    <button type='submit' onClick={saveChange} className={EnableEdit ? ' bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white ml-1 ' : 'hidden'}>Save</button>
+               </div>
             </div>
 
             <div className='flex-col gap-2 items-center p-5 '>
@@ -74,11 +78,15 @@ function TodoList() {
                     {
                         Todos.length === 0 ? <div className=' mt-12 text-center font-bold text-6xl text-gray-600 '>Todo List is empty</div> :
                             Todos.map((item, index) => (
-                                <div key={item.id} className=' bg-gray-300 flex m-2 justify-between rounded-xl items-center p-2'>
-                                    <p>{item.Todo}</p>
-                                    <div>
-                                        <button onClick={e => handleEdit(index, item.id)} className=' bg-purple-800 p-2 rounded-lg font-bold ml-1 border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white'>Edit</button>
-                                        <button onClick={e => handleDelete(item.id)} className=' bg-purple-800 p-2 rounded-lg font-bold ml-1 border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white'>Delete</button>
+                                <div key={item.id} className=' bg-gray-300 flex justify-between m-2 rounded-xl items-center p-2'>
+                                    <p className=' max-w-[680px] shrink break-all '>{item.Todo}</p>
+                                    <div className=' min-w-[105px] flex flex-none'>
+                                        <button onClick={e => handleEdit(index, item.id)} className='bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white m-1' >
+                                            <img className=' hover:invert ' src={edit} alt="EDIT" />
+                                        </button>
+                                        <button onClick={e => handleDelete(item.id)} className='bg-purple-800 p-2 rounded-lg font-bold border-2 border-purple-900 hover:bg-purple-900 hover:text-white hover:border-2 active:border-white m-1'>
+                                            <img className=' hover:invert' src={remove} alt="DELETE" />
+                                        </button>
                                     </div>
                                 </div>
                             ))
